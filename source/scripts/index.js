618dc3016a2f36770c7157db3e622e7631eee9d4
Dmitriy <dimanich12512@gmail.com>
2025-02-15 18:02:43 +1000

    Сделал блок с фильтром каталога для мобильной версии

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,20 @@
 /* в этот файл добавляет скрипты*/
+console.log(1)
+const sliderCatalog = document.querySelector('.catalog__filter-price-slider');
+const minPriceInput = document.getElementById('min-price');
+const maxPriceInput = document.getElementById('max-price')
+
+noUiSlider.create(sliderCatalog, {
+  range: {
+    min: 0,
+    max: 1000,
+  },
+  start: [0,900],
+  step: 50,
+  connect: true,
+}) ;
+
+sliderCatalog.noUiSlider.on('update', (values) => {
+  minPriceInput.value = Math.trunc(values[0]);
+  maxPriceInput.value = Math.trunc(values[1]);
+})
